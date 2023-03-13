1b144707f72147abbabd318f83646ffdccf8455c
Corbin C Corbius <126821969+Sneakyweaze@users.noreply.github.com>
2023-03-13 01:59:19 +0000

    added question

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-
+#this is the list for the questions.
+Questions = ["How Many Gyms are there in the school?","What class block is the science block?","What is the name of the area between B Block, The hall and the admin building?",]
 #this list is all the answers for the questions.
 Answers  = ["3","F Block","A Quad",]
