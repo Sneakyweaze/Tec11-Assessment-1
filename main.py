833b564f51ead71156a1a1bdf2c82087227388dc
Corbin C Corbius <126821969+Sneakyweaze@users.noreply.github.com>
2023-03-14 23:05:24 +0000

    made the code say hello

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 Questions = ["How Many Gyms are there in the school?","What class block is the science block?","What is the name of the area between B Block, The hall and the admin building?",]
 #this list is all the answers for the questions.
 Answers  = ["3","F Block","A Quad",]
+#this is the varible for what question/answer is
+qanum = 0 
+#this imports time and says hello;then wait o.5 seconds 
+import time
+print("hello user")
+time.sleep(0.5)
+#asks user for name
