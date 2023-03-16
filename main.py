3916676e0ebe20d6f9c5b1c5b68e2353a2b33b50
Corbin C Corbius <126821969+Sneakyweaze@users.noreply.github.com>
2023-03-16 20:36:13 +0000

    adds quiz loop

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 #this is the list for the questions.
-Questions = ["How Many Gyms are there in the school?","What class block is the science block?","What is the name of the area between B Block, The hall and the admin building?",]
+Question = ["How Many Gyms are there in the school? ","What class block is the science block? ","What is the name of the area between B Block, The hall and the admin building ",]
 #this list is all the answers for the questions.
-Answers  = ["3","F Block","A Quad",]
+Answer  = ["3","F Block","A Quad",]
 #this is the varible for what question/answer is
 qanum = 0 
-#this imports time and says hello;then wait o.5 seconds 
+#this is the varible for how many point you have 
+points = 0
+#this imports time and says hello ; then waits o.5 seconds 
 import time
-print("hello user")
+print("Hello User")
 time.sleep(0.5)
-#asks user for name
+#asks user for name, saves as username 
+username = input("What is your Name? ")
+#say that the quiz is starting, prosent question[qanum]
+while qanum != 4:
+ userasr = input(Question[qanum])
+ if userasr == Answer[qanum]:
+   print("You're Correct!")
+   points += 1
+   qanum += 1
+   continue
+ else:
+   print("That Is Incorrect, the answer was",Answer[qanum],"!")
+   qanum += 1
+   continue
+
+
+  
+
