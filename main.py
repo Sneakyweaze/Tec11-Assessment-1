0824e815c397c8057a43557b976038a34e6ef8bb
Corbin C Corbius <126821969+Sneakyweaze@users.noreply.github.com>
2023-03-20 01:53:19 +0000

    added the breaking sequince to end the code when there are no more questions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #this is the list for the questions.
-Question = ["How Many Gyms are there in the school? ","What class block is the science block? ","What is the name of the area between B Block, The hall and the admin building ",]
+Question = ["How Many Gyms are there in the school? ","What class block is the science block? ","What is the name of the area between B Block, The hall and the admin building? ",]
 #this list is all the answers for the questions.
 Answer  = ["3","F Block","A Quad",]
 #this is the varible for what question/answer is
@@ -13,18 +13,22 @@ time.sleep(0.5)
 #asks user for name, saves as username 
 username = input("What is your Name? ")
 #say that the quiz is starting, prosent question[qanum]
-while qanum != 4:
+while qanum <= 4:
  userasr = input(Question[qanum])
  if userasr == Answer[qanum]:
-   print("You're Correct!")
-   points += 1
-   qanum += 1
-   continue
+    print("You're Correct!")
+    points += 1
+    qanum += 1
+    if qanum == 3: 
+     break
+    else:
+     continue
  else:
    print("That Is Incorrect, the answer was",Answer[qanum],"!")
    qanum += 1
-   continue
+   if qanum == 3: 
+     break
+   else:
+    continue
 
-
-  
-
+print("hello")
