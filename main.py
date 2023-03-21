51eacad9e68278b64a05f71a9efec8545e30e990
Corbin C Corbius <126821969+Sneakyweaze@users.noreply.github.com>
2023-03-21 00:35:09 +0000

    added functions

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #this is the list for the questions.
-Question = ["How Many Gyms are there in the school? ","What class block is the science block? ","What is the name of the area between B Block, The hall and the admin building? ",]
+Question = ["How Many Gyms are there in the school? ","What class block is the science block? ","What is the name of the area between B Block, The hall and the admin building? ",""]
 #this list is all the answers for the questions.
 Answer  = ["3","F Block","A Quad",]
 #this is the varible for what question/answer is
 qanum = 0 
+#this is the function that says somtheing is correct
+def yes_function():
+  print("That Is Correct!")
+#this is the function that says somtheing is incorrect
+def no_function():
+  print("That Is Incorrect!")
 #this is the varible for how many point you have 
 points = 0
 #this imports time and says hello ; then waits o.5 seconds 
@@ -13,13 +19,13 @@ time.sleep(0.5)
 #asks user for name, saves as username 
 username = input("What is your Name? ")
 time.sleep(0.5)
-print("sample text")
+print("Please Use A Capitel Letter At The Start Of Each Word")
 time.sleep(0.5)
 #say that the quiz is starting, prosent question[qanum]
 while qanum <= 4:
  userasr = input(Question[qanum])
  if userasr == Answer[qanum]:
-    print("You're Correct!")
+    yes_function()
     points += 1
     qanum += 1
     if qanum == len(Answer): 
@@ -27,11 +33,13 @@ while qanum <= 4:
     else:
      continue
  else:
-   print("Sorry, That Is Incorrect")
+   no_function()
    qanum += 1
    if qanum == len(Answer): 
      break
    else:
     continue
 
-print("hello")
+print("You Have Finshed The Quiz.")
+time.sleep(0.5)
+print("You Got ",points,"out of",len(Question),"Questions Correct.")
