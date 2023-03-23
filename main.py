f2c699c54efacac6c158e36eaa6f9615a1eb3d57
Corbin C Corbius <126821969+Sneakyweaze@users.noreply.github.com>
2023-03-23 20:48:56 +0000

    added more questions and play again code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #this is the list for the questions.
-Question = ["How Many Gyms are there in the school? ","What class block is the science block? ","What is the name of the area between B Block, The hall and the admin building? ","Where is The Tennis Corts? "]
+Question = ["How Many Gyms are there in the school? ","What class block is the science block? ","What is the name of the area between B Block, The hall and the admin building? ","Where is The Tennis Corts? ","What subject is studied in G Block? ","What do you learn in K block? ","What do you learn in M block? ","What block do you learn Social Studies? ","What do you learn in A block? ","What is the class number for referral? "]
 #this list is all the answers for the questions.
-Answer  = ["three","f block","a quad","behind gym one",]
+Answer  = ["three","f block","a quad","behind gym one","english","languages","music","d block","art","f eleven"]
 #this is the varible for what question/answer is
 qanum = 0 
 #this is the varible for if the loop is on or not
@@ -20,17 +20,20 @@ def formatNumber(dec2):
     return int(dec2)
   else:
     return dec2
-#this imports time and says hello ; then waits o.5 seconds 
+
 import time
-print("Hello User")
-time.sleep(0.5)
+#this is where the code will be reset if the user wants to play again
+while loop == "yes":
+#says hello and then waits o.5 seconds 
+ print("Hello User")
+ time.sleep(0.5)
 #asks user for name, saves as username 
-username = input("What is your Name? ")
-time.sleep(0.5) 
-print("Please Use A Lowercase Letter At The Start Of Each Word and don't use numbers in your answers")
-time.sleep(0.5)
+ username = input("What is your Name? ")
+ time.sleep(0.5) 
+ print("Please Use A Lowercase Letter At The Start Of Each Word and don't use numbers in your answers")
+ time.sleep(0.5)
 #say that the quiz is starting, prosent question[qanum]
-while qanum == len(Answer) + 1:
+ while qanum != len(Answer) + 1:
   userasr = input(Question[qanum])
   if userasr == Answer[qanum]:
     yes_function()
@@ -48,15 +51,24 @@ while qanum == len(Answer) + 1:
    else:
     continue
 
-print("You Have Finshed The Quiz.")
-time.sleep(0.5)
-print(username,",You Got ",points,"out of",len(Question),"Questions Correct.")
-dec1 = points / len(Answer)
-dec2 = dec1 * 100
-time.sleep(0.5)
-print("Thats a",formatNumber(dec2),"% success rate ")
-loop = input("do you want to play again? ")
-if loop == "yes":
+# when out of questions, the code will show score and ask to play again.
+ print("You Have Finshed The Quiz.")
+ time.sleep(0.5)
+ print(username,",You Got ",points,"out of",len(Question),"Questions Correct.")
+ dec1 = points / len(Answer)
+ dec2 = dec1 * 100
+ time.sleep(0.5)
+ print("Thats a",formatNumber(dec2),"% success rate ")
+ time.sleep(0.5)
+ loop = input("do you want to play again? ")
+ if loop == "yes":
   qanum = 0
-else:
+  time.sleep(0.5)
+  print("resetting...")
+  time.sleep(0.5)
+  print("            ")
+  time.sleep(0.5)
+  continue 
+ else:
+  time.sleep(0.5)
   print("good bye")
