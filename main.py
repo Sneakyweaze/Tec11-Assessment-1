ccda5524773c3d7315c78dab797b0e334f2a5a34
Corbin C Corbius <>
2023-04-03 02:32:30 +0000

    added secret code part :)

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 #this adds OS, which is used to clear the console when to code resets
 import os
+
 #This is the list for the questions.
 Question = ["How Many Gyms are there in the school? ","What class block is the science block? ","What is the name of the area between B Block, The hall and the admin building? ","Where are The Tennis Courts? ","What subject is studied in G Block? ","What do you learn in K block? ","What do you learn in M block? ","What block do you learn Social Studies? ","What do you learn in A block? ","What is the class number for referral? "]
+
 #This list is all the answers for the questions.
 Answer  = ["three","f block","a quad","behind gym one","english","languages","music","d block","art","f eleven"]
+
 #this is the variable for what question/answer is
 qanumber = 0 
+
 #this is the variable for if the loop is on or not
 loop = "yes"
+
 #this is the function that says something is correct
 def yes_function():
   print("That Is Correct!")
+  
 #this is the function that says something is incorrect
 def no_function():
   print("That Is Incorrect!")
+  
 #this is the variable for how many point you have 
 points = 0
+
 #this function removes trailing .0 from the percentage of success rate
 def formatNumber(dec2):
   if dec2 % 1 == 0:
@@ -24,23 +32,34 @@ def formatNumber(dec2):
     return dec2
 
 import time
+
 #this is where the code will be reset if the user wants to play again
 while loop == "yes":
+  
 #says hello and then waits o.5 seconds 
  print("Hello User")
  time.sleep(0.5)
  print("\n")
-#asks user for name, saves as username: states the rules of the quiz 
+  
+#asks user for name, saves as username:
  username = input("What is your Name? ")
  time.sleep(0.5) 
  print("\n")
- print("Please Use A Lowercase Letter At The Start Of Each Word and don't use numbers in your answers")
- time.sleep(0.5)
-#say that the quiz is starting, prosent question[qanumber]
- while qanumber != len(Answer) + 1:
-  print("\n")
+
+#this is a secret, don't read this.
+ if username == "Ewqasd346":
+  print("Nice Try, No Secret code Here.")
+ else: 
+# This says the rules of the quiz
+  print("Please don't use numbers in your answers")
   time.sleep(0.5)
-  userasr = input(Question[qanumber])
+  
+#say that the quiz is starting, prosent question[qanumber]
+  while qanumber != len(Answer) + 1:
+   print("\n")
+   time.sleep(0.5)
+   userasr = input(Question[qanumber])
+   
 #if question is correct,the code adds one to points and qanumber: continues loop
   if userasr.lower() == Answer[qanumber]:
     print("\n")
@@ -53,6 +72,7 @@ while loop == "yes":
      time.sleep(0.5)
     else:
      continue
+      
 #if question is incorrect,the code doesn't adds one to points and adds one to qanumber: continues loop
   else:
    print("\n")
@@ -69,17 +89,20 @@ while loop == "yes":
  print("You Have Finished The Quiz.")
  time.sleep(0.5)
  print("\n")
+  
 # this shows how many points the user got out of how many questions there are
  print(username,",You Got ",points,"out of",len(Question),"Questions Correct.")
  dec1 = points / len(Answer)
  dec2 = dec1 * 100
  time.sleep(0.5)
  print("\n")
+  
 # this shows the percentage of what questions the user correct to incorrect
  print("Thats a",formatNumber(dec2),"% success rate ")
  time.sleep(0.5)
  print("\n")
  loop = input("do you want to play again? ")
+  
 #if the user responds with yes, the code will reset.
  if loop == "yes":
   qanumber = 0
@@ -88,10 +111,12 @@ while loop == "yes":
   print("\n")
   print("resetting...")
   time.sleep(1)
+   
 # this clears the console
   os.system('clear')
   time.sleep(0.5)
-  continue 
+  continue
+   
 #if the user responds with something that isn't yes, the code will end.
  else:
   time.sleep(0.5)
